40d6a7d623e7af87c8d8991568efb9b9123d5e59
FredProgra <alfredoqooo@gmail.com>
2023-10-01 15:28:53 -0500

    init commit

---
+++ src/main/java/edu/pe/cibertec/springs_semana6/SpringsSemana6Application.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.springs_semana6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsSemana6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsSemana6Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/cibertec/springs_semana6/SpringsSemana6ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.cibertec.springs_semana6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsSemana6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
